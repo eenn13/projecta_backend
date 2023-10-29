bc6f0f9915408dff22e6e87214ea07e091ba5779
Eren Turkel <110343430+erenturkel13@users.noreply.github.com>
2023-10-29 14:08:36 +0300

    remove listen

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const init = require('./init');
 const app = express();
 const cors = require("cors");
-const port = require("./config").PORT;
 
 
 init();
@@ -10,7 +9,3 @@ app.use(cors());
 app.use('/user', require('./controllers/UserController'));
 app.use('/share', require('./controllers/ShareController'));
 app.use('/transaction', require('./controllers/TransactionController'));
-
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-});
