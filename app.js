017776dfc7b504b92b8a12fe2c5338695d07687b
Eren Turkel <110343430+erenturkel13@users.noreply.github.com>
2023-10-29 14:22:47 +0300

    serverjs file

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ app.use(cors());
 app.use('/user', require('./controllers/UserController'));
 app.use('/share', require('./controllers/ShareController'));
 app.use('/transaction', require('./controllers/TransactionController'));
+
+module.exports = app;
